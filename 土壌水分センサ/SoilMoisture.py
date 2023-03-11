65ab49b1d1e01ee2dbe7709d1e4828c31f46bffd
Else156 <21ec109@ms.dendai.ac.jp>
2023-03-11 21:41:06 +0900

    API仕様書を作成

--- 土壌水分センサ/SoilMoisture.py
+++ 土壌水分センサ/SoilMoisture.py
@@ -1,4 +1,13 @@
 # -- coding: utf-8 --
+"""
+Module Name:<br> 
+電流値取得プログラム<br><br>
+Description:<br> 
+電流センサを使って電圧、電流、電力、シャント電圧の値を取得できるプログラム。動作確認用実行ファイルはSoilMoistureTest.py<br><br>
+Library:<br>
+from ina219 import INA219とfrom ina219 import DeviceRangeError<br>
+「sudo pip3 install pi-ina219」でダウンロードする<br><br>
+"""
 from ina219 import INA219
 from ina219 import DeviceRangeError
 import logging
@@ -9,6 +18,16 @@ INA219_ADDRESS = 0x40
 
 #値の取得
 def SoilMois_result():
+    """
+    電流センサ(INA219)を使って電圧、電流、電力、シャント電圧の値を返却するプログラム。
+
+    Returns
+    -------
+    list
+            リスト形式で、[電圧（V）、電流（mA）、電力（mW）、シャント電圧（V）]を返す。
+    bool
+            OSErrorが発生した場合はTrueを返す。
+    """
     try:
         ina = INA219(SHUNT_OHMS, MAX_EXPECTED_AMPS, address=INA219_ADDRESS, log_level=logging.INFO)
         ina.configure()
