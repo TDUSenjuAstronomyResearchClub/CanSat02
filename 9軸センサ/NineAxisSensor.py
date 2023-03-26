9ef53b800eb22b3432d658a9fa261dcc0d7d7d25
Else156 <21ec109@ms.dendai.ac.jp>
2023-03-26 20:32:17 +0900

    9軸センサのプログラム作成

---
+++ 9軸センサ/NineAxisSensor.py
@@ -0,0 +1,85 @@
+"""
+Module Name:<br> 
+9軸センサ<br><br>
+Description:<br> 
+加速度・角速度・方位角を求めるプログラム。動作確認用実行ファイルはNineAxisSensorTest.py<br><br>
+Library:<br>
+BMX055<br>
+pip install bmx055<br><br>
+"""
+
+import BMX055
+import math
+
+class BMX055Sensor:
+    """
+    BMX055センサを制御し、加速度・角速度・方位角を求めるクラス。
+    """
+    def __init__(self, declination=0):
+        """
+        BMX055センサを初期化する。
+
+        :param declination: 地磁気偏角（単位：度）。省略時はゼロを指定する。
+        """
+        self.bmx055 = BMX055.BMX055()
+        self.declination = declination
+
+    def get_acceleration(self):
+        """
+        加速度を取得する。
+        Returns
+
+        -------
+        list
+             加速度（x, y, z）（単位:m/s^2）
+        bool
+            OSErrorが発生した場合はTrueを返す。
+        """
+        try:
+            raw_accel = self.bmx055.get_accel_data()
+            return [x / 1000 for x in raw_accel]
+        except OSError:
+            return True
+
+    def get_gyroscope(self):
+        """
+        角速度を取得する。
+
+        Returns
+        -------
+        list
+                角速度（x, y, z）（単位:rad/s）
+        bool
+            OSErrorが発生した場合はTrueを返す。
+        """
+        try:
+            raw_gyro = self.bmx055.get_gyro_data()
+            return [math.radians(x) for x in raw_gyro]
+        except OSError:
+            return True
+
+    def get_magnetic_heading(self):
+        """
+        地磁気センサから方位角を計算する。
+
+        Returns
+        -------
+        list
+                方位角（単位：度）
+        bool
+            OSErrorが発生した場合はTrueを返す。
+        """
+        try:
+            raw_mag = self.bmx055.get_mag_data()
+
+            # 地磁気偏角を適用する
+            heading = math.atan2(raw_mag[1], raw_mag[0]) + math.radians(self.declination)
+
+            # 方位角を0から360度の範囲にする
+            heading = math.degrees(heading)
+            if heading < 0:
+                heading += 360.0
+
+            return heading
+        except OSError:
+            return True
---
+++ 9軸センサ/NineAxisSensorTest.py
@@ -0,0 +1,16 @@
+import NineAxisSensor
+import time
+
+while True:
+    sensor = NineAxisSensor.BMX055Sensor(declination=7.8) # 磁気偏角（2021年時点で秋田県能代市の磁気偏角は7.8度とされている）
+
+    acceleration = sensor.get_acceleration()
+    print(f"Acceleration: {acceleration} m/s^2")
+
+    gyroscope = sensor.get_gyroscope()
+    print(f"Gyroscope: {gyroscope} rad/s")
+
+    magnetic_heading = sensor.get_magnetic_heading()
+    print(f"Magnetic heading: {magnetic_heading} degrees")
+
+    time.sleep(1)
