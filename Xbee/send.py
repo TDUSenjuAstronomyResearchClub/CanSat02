6a2cf0e01f75c4b9feecdf03b2793a2ee57f65a7
taichi <106682320+Else156@users.noreply.github.com>
2023-04-11 09:23:39 +0900

    直線距離と方位角対応

--- Xbee/send.py
+++ Xbee/send.py
@@ -2,6 +2,7 @@ import json
 import datetime
 
 import serial
+import time
 
 from ..gps import GPS
 from ..nineAxisSensor.NineAxis import BMX055Sensor as NineAxis
@@ -10,7 +11,7 @@ from ..pressure import BarometricPress
 from ..battery import Battery
 from ..distance import Distance
 
-import Runing #走行プログラムのソースファイル
+import Running #走行プログラムのソースファイル
 
 # ポート設定
 PORT = '/dev/ttyUSB0'
@@ -23,8 +24,10 @@ start_time = 'sending' + dt_start.strftime('%Y年%m月%d日_%H時%M分%S秒')
 
 while True:
     gps_data = GPS.get_gps_data()
-    
-    gps_distance = GPS.calculate_distance_bearing()
+    lat_lon = Running.SeeValue()    #走行プログラムに定義されているサンプル採取地点とゴール地点の緯度経度値を持ってくる
+    sample_distance = GPS.calculate_distance_bearing(lat_lon[0],lat_lon[1]) 
+    goal_distance = GPS.calculate_distance_bearing(lat_lon[2],lat_lon[3])
+
     nineAxis = NineAxis()
     bme280 = Temperature.Temperature_result()  # 温湿度気圧センサデータ
     lps25hb = BarometricPress.get_pressure_altitude_temperature()  # 気圧センサ
@@ -33,14 +36,14 @@ while True:
             "緯度": gps_data[0],
             "経度": gps_data[1],
             "海抜": gps_data[2],
-            # todo 距離計算モジュールがどこにあるかわからない
+
             "サンプル": {
-                "直線距離": 1,
-                "方位角": 2
+                "直線距離": sample_distance[0],
+                "方位角": sample_distance[1]
             },
             "ゴール": {
-                "直線距離": 1,
-                "方位角": 2
+                "直線距離": goal_distance[0],
+                "方位角": goal_distance[1]
             }
         },
         "9軸": {
@@ -74,3 +77,5 @@ while True:
     # シリアルにjsonを書き込む
     ser.write(bytes(json.load(f), 'utf-8'))
     f.close()
+
+    time.sleep(1)
