fbfc7bcc5bd8b7a7938dae01d79fcafc5b6a2153
Jinnosuke Kato <96569057+jinnosukeKato@users.noreply.github.com>
2023-04-14 09:53:12 +0900

    型を指定

--- gps/gps.py
+++ gps/gps.py
@@ -1,13 +1,9 @@
-"""
-Module Name: <br>
-緯度経度・海抜・磁器偏角・直線距離・方位角取得プログラム<br><br>
-Description:<br>
-gps.pyから緯度経度、海抜、磁器偏角を取得し、ゴールと機体との直線距離と方位角を求めるプログラム<br><br>
+"""緯度経度・海抜・磁気偏角・直線距離・方位角取得モジュール
 
-Library:<br>
-serial<br>
-「pip install --upgrade pip」を行いpipを最新版にします<br>
-「pip install pyserial」このコマンドを打つことでpyserialを導入できます<br>
+GPSから緯度経度・海抜・磁気偏角を取得し、そこから2地点間の距離を計算します
+
+使用しているライブラリ:
+    pyserial
 """
 
 import serial
@@ -15,20 +11,14 @@ import time
 from math import radians, sin, cos, atan2, sqrt, pi
 
 
-def get_gps_data():
-    """
-    GPSデータを取得する関数
+def get_gps_data() -> list[float]:
+    """GPSデータを取得する関数
 
-    Returns
-    -------
-    lat : float
-            度分形式の緯度
-    lon : float
-            度分形式の経度
-    alt : float
-            海抜
-    declination : float
-            磁気偏角(度)
+    Returns:
+        list[float]: 緯度(ddmm.mm), 経度(dddmm.mm), 海抜(m), 磁気偏角(度)
+
+    Raises:
+        SerialError: シリアル通信時に発生する可能性がある
     """
 
     ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=1)
@@ -59,26 +49,23 @@ def get_gps_data():
                 break
 
     ser.close()
-    return lat, lon, alt, declination
+    return [lat, lon, alt, declination]
 
 
-def lat_conv_deg_min_to_decimal(lat, direction):
-    """
-    緯度を度分形式から10進数形式に変換する関数
+def lat_conv_deg_min_to_decimal(lat: str, direction: str) -> float:
+    """緯度を度分形式から10進数形式に変換する関数
 
     GPGGA緯度フォーマット: ddmm.mm
     https://gpsd.gitlab.io/gpsd/NMEA.html#_gga_global_positioning_system_fix_data
-    Args
-    -------
-    lat : str
-            度分形式の緯度
-    direction : str
-            方向（N, Sのどちらか）
 
-    Returns
-    -------
-    degree : float
-            10進数形式の経度
+    Args:
+        lat: str
+                度分形式の緯度
+        direction: str
+                方向（N, Sのどちらか）
+
+    Returns:
+        float: 10進数形式の経度
     """
     d = float(lat[:2])
     m = float(lat[2:])
@@ -90,23 +77,20 @@ def lat_conv_deg_min_to_decimal(lat, direction):
     return decimal
 
 
-def lon_conv_deg_min_to_decimal(lon, direction):
-    """
-    経度を度分形式から10進数形式に変換する関数
+def lon_conv_deg_min_to_decimal(lon: str, direction: str) -> float:
+    """経度を度分形式から10進数形式に変換する関数
 
     GPGGA経度フォーマット: dddmm.mm
     https://gpsd.gitlab.io/gpsd/NMEA.html#_gga_global_positioning_system_fix_data
-    Args
-    -------
-    lon : str
-            度分形式の経度
-    direction : str
-            方向（E, Wのいずれか）
 
-    Returns
-    -------
-    degree : float
-            10進数形式の経度
+    Args:
+        lon : str
+                度分形式の経度
+        direction : str
+                方向（E, Wのいずれか）
+
+    Returns:
+        float: 10進数形式の経度
     """
     d = float(lon[:3])
     m = float(lon[3:])
@@ -118,23 +102,17 @@ def lon_conv_deg_min_to_decimal(lon, direction):
     return decimal
 
 
-def calculate_distance_bearing(lat, lon):
-    """
-    機体の現在地点から指定された地点の緯度経度までの直線距離と方位角を計算する関数
+def calculate_distance_bearing(lat: float, lon: float) -> list[float]:
+    """機体の現在地点から指定された地点の緯度経度までの直線距離と方位角を計算する関数
 
-    Args
-    -------
-    lat : float
+    Args:
+        lat: float
             目的地の緯度
-    lon : float
+        lon: float
             目的地の経度
 
-    Returns
-    -------
-    distance : float
-            2地点間の直線距離 
-    bearing : float
-            2地点間の方位角 
+    Returns:
+        list[float]: 2地点間の直線距離, 2地点間の方位角
     """
     r = 6371  # 地球の半径（km）
     # gpsの緯度経度・磁器偏角値を取得
@@ -161,4 +139,4 @@ def calculate_distance_bearing(lat, lon):
     # 磁気偏角の補正
     bearing = (bearing + declination) % 360
 
-    return distance, bearing
+    return [distance, bearing]
