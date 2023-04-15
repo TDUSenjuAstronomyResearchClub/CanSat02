c56631f6b5dc7465f642d596a78f3e9fc867b581
Jinnosuke Kato <96569057+jinnosukeKato@users.noreply.github.com>
2023-04-15 19:02:16 +0900

    コメントスタイルを変更

--- soilmoisture/soil_moisture.py
+++ soilmoisture/soil_moisture.py
@@ -1,22 +1,15 @@
-"""
-Module Name:<br> 
-土壌水分量測定プログラム<br><br>
-Description:<br> 
-土壌水分センサを使って土壌水分率を取得できるプログラム<br><br>
-Library:<br>
-board<br>
-pip install board<br><br>
-busio<br>
-pip install adafruit-blinka<br><br>
-Seesaw<br>
-pip install adafruit-circuitpython-seesaw<br><br>
+"""土壌水分量測定モジュール
 
+土壌水分センサを使って土壌水分率を取得するモジュール
+
+使用しているライブラリ:
+    Adafruit-Blinka
+    adafruit-circuitpython-seesaw
 """
 from time import sleep
 import board
 import busio
 from adafruit_seesaw.seesaw import Seesaw
-import pdoc
 
 
 i2c_bus = busio.I2C(board.SCL, board.SDA)
@@ -24,25 +17,18 @@ ss = Seesaw(i2c_bus, addr=0x36)
 
 
 def get_soil_moisture():
-    """
-    土壌水分センサ（Adafruit STEMMA Soil Sensor - I2C CapacitiveMoisture Sensor）から土壌水分率を取得します。
+    """土壌水分センサ（Adafruit STEMMA Soil Sensor - I2C CapacitiveMoisture Sensor）から土壌水分率を取得します
 
-    Returns
-    -------
-    float
-        0から100の間のfloat値としての土壌水分率
+    Returns:
+        float: 0.0から100.0の間の土壌水分率
 
-    Exception
-        OSErrorが発生した場合はエラーを返す。
+    Raises:
+        OSError: I2C通信が正常に行えなかった際に発生
     """
     try:
         moisture = ss.moisture_read()
         sleep(0.1)  # センサが安定するまで 100 ms 待つ
         return 100 - ((moisture / 65535.0) * 100)
     
-    except OSError as e:
-        return e
-
-
-# Generate API documentation using pdoc.
-pdoc.pdoc(get_soil_moisture)
+    except OSError:
+        raise OSError
