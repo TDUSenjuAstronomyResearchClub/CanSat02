6f603a081d897750a2eca2fed1b01bae81e0aad2
taichi <21ec109@ms.dendai.ac.jp>
2023-04-15 19:03:07 +0900

    例外処理の方式をsend.pyに対応させた

--- distance/distance.py
+++ distance/distance.py
@@ -10,8 +10,11 @@ import time
 from ..temperature import temperature
 
 # 測定環境温度
-temp = temperature.temperature_result()  # 温湿度気圧センサから現在の温度値を呼び出す
-TEMP = temp[0]
+try:
+    temp = temperature.temperature_result()  # 温湿度気圧センサから現在の温度値を呼び出す
+    TEMP = temp[0]
+except OSError: #OSerrerが発生した場合は、25度として計算をしていく
+    TEMP = 25
 
 # GPIO設定
 GPIO.setwarnings(False)
@@ -30,30 +33,26 @@ def distance_result():
     -------
     str
             str型で距離(cm)を返却する
-    OSError
-            OSErrorが発生した場合はエラー文を返す。
     """
-    try:
-        # トリガ信号出力
-        GPIO.output(17, GPIO.HIGH)
-        time.sleep(0.00001)
 
-        GPIO.output(17, GPIO.LOW)
+    # トリガ信号出力
+    GPIO.output(17, GPIO.HIGH)
+    time.sleep(0.00001)
+    GPIO.output(17, GPIO.LOW)
 
-        # 返送HIGHレベル時間計測
-        while GPIO.input(27) == GPIO.LOW:
-            soff = time.time()  # LOWレベル終了時刻
+    # 返送HIGHレベル時間計測
+    while GPIO.input(27)==GPIO.LOW: 
+        soff = time.time()  # LOWレベル終了時刻
 
-        while GPIO.input(27) == GPIO.HIGH:
-            son = time.time()  # HIGHレベル終了時刻
+    while GPIO.input(27) == GPIO.HIGH:
+        son = time.time()  # HIGHレベル終了時刻
 
-        # HIGHレベル期間の計算
-        clc = son - soff
+    # HIGHレベル期間の計算
+    clc = son - soff
 
-        # 時間から距離に変換(TEMPは測定環境温度)
-        clc = clc * (331.50 + (0.6 * TEMP)) / 2 * 100
+    # 時間から距離に変換(TEMPは測定環境温度)
+    clc = clc * (331.50 + (0.6 * TEMP)) / 2 * 100
 
 
-        return str(clc)
-    except TypeError as e:
-        return e
+    return str(clc)
+
