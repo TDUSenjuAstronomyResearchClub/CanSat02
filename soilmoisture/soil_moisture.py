fe8a5e8e5401bada2283356f4268a03c6739a285
Jinnosuke Kato <96569057+jinnosukeKato@users.noreply.github.com>
2023-04-15 19:02:41 +0900

    返り値の型を指定

--- soilmoisture/soil_moisture.py
+++ soilmoisture/soil_moisture.py
@@ -16,7 +16,7 @@ i2c_bus = busio.I2C(board.SCL, board.SDA)
 ss = Seesaw(i2c_bus, addr=0x36)
 
 
-def get_soil_moisture():
+def get_soil_moisture() -> float:
     """土壌水分センサ（Adafruit STEMMA Soil Sensor - I2C CapacitiveMoisture Sensor）から土壌水分率を取得します
 
     Returns:
