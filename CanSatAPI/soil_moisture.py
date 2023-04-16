31bf1f72a6cd78bb3374d9e08c771b0bb14bb0a9
Jinnosuke Kato <96569057+jinnosukeKato@users.noreply.github.com>
2023-04-16 17:23:06 +0900

    クラス化

--- CanSatAPI/soil_moisture.py
+++ CanSatAPI/soil_moisture.py
@@ -12,23 +12,28 @@ import busio
 from adafruit_seesaw.seesaw import Seesaw
 
 
-i2c_bus = busio.I2C(board.SCL, board.SDA)
-ss = Seesaw(i2c_bus, addr=0x36)
+class SoilMoistureSensor:
+    """土壌水分センサ(Adafruit STEMMA Soil Sensor - I2C CapacitiveMoisture Sensor)を扱うクラス
 
-
-def get_soil_moisture() -> float:
-    """土壌水分センサ（Adafruit STEMMA Soil Sensor - I2C CapacitiveMoisture Sensor）から土壌水分率を取得します
-
-    Returns:
-        float: 0.0から100.0の間の土壌水分率
-
-    Raises:
-        OSError: I2C通信が正常に行えなかった際に発生
+    製品ページ: https://learn.adafruit.com/adafruit-stemma-soil-sensor-i2c-capacitive-moisture-sensor
     """
-    try:
-        moisture = ss.moisture_read()
-        sleep(0.1)  # センサが安定するまで 100 ms 待つ
-        return 100 - ((moisture / 65535.0) * 100)
-    
-    except OSError:
-        raise OSError
+    def __init__(self):
+        i2c_bus = busio.I2C(board.SCL, board.SDA)
+        self.seesaw = Seesaw(i2c_bus, addr=0x36)
+
+    def get_soil_moisture(self) -> float:
+        """土壌水分率(%)を取得します
+
+        Returns:
+            float: 0.0から100.0の間の土壌水分率(%)
+
+        Raises:
+            OSError: I2C通信が正常に行えなかった際に発生
+        """
+        try:
+            moisture = self.seesaw.moisture_read()
+            sleep(0.1)  # センサが安定するまで 100 ms 待つ
+            return 100 - ((moisture / 65535.0) * 100)
+
+        except OSError:
+            raise OSError
