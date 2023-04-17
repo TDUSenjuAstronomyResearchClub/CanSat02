f9341d63ed8a3a0da70a6fcfbfcee2fc3096e644
jinnosukeKato <jinnosukeKato@gmail.com>
2023-04-18 00:28:35 +0900

    mainブランチを指定

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,4 +44,4 @@ def linkcode_resolve(domain, info):
     if not info['module']:
         return None
     filename = info['module'].replace('.', '/')
-    return "https://github.com/Else156/CanSat02/%s.py" % filename
+    return "https://github.com/Else156/CanSat02/tree/main/%s.py" % filename
