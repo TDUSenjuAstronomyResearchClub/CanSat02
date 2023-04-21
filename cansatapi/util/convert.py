671f0e5b21e7f702d14f294ccbe668b13f24de45
Jinnosuke Kato <96569057+jinnosukeKato@users.noreply.github.com>
2023-04-21 09:56:47 +0900

    必要のない単位指定を削除

--- cansatapi/util/convert.py
+++ cansatapi/util/convert.py
@@ -32,26 +32,30 @@ def raw_ang_rate_to_ang_per_s(data: list[float], range_abs: int) -> list[float]:
 
 
 def ut_to_azimuth(x: float, y: float) -> float:
-    """3軸の地磁気[μT]から方位角[°]を計算する
+    """3軸の地磁気から方位角[°]を計算する
+
+    地磁気の比から方位角を算出するので地磁気の単位は問いません
 
     Args:
-        x: X軸の地磁気[μT]
-        y: Y軸の地磁気[μT]
+        x: X軸の地磁気
+        y: Y軸の地磁気
 
     Returns:
         float: 方位角[°]
     """
     # todo: センサーの傾きを考慮した式にする
     # todo: キャリブレーションを追加する
-    return math.atan(x/y)
+    return math.atan(x / y)
 
 
 def acceleration_to_roll(x: float, y: float) -> float:
-    """X, Y軸の加速度[m/s^2]からロール角[rad]を計算する
+    """X, Y軸の加速度からロール角[rad]を計算する
+
+    加速度の比からロール角を算出するので加速度の単位は問いません
 
     Args:
-        x (float): X軸の加速度[m/s^2]
-        y (float): Y軸の加速度[m/s^2]
+        x (float): X軸の加速度
+        y (float): Y軸の加速度
 
     Returns:
         float: ロール角[rad]
@@ -60,12 +64,14 @@ def acceleration_to_roll(x: float, y: float) -> float:
 
 
 def acceleration_to_pitch(x: float, y: float, z: float) -> float:
-    """X, Y, Z軸の加速度[m/s^2]からピッチ角[rad]を計算する
+    """X, Y, Z軸の加速度からピッチ角[rad]を計算する
+
+    加速度の比からピッチ角を算出するので加速度の単位は問いません
 
     Args:
-        x (float): X軸の加速度[m/s^2]
-        y (float): Y軸の加速度[m/s^2]
-        z (float): Z軸の加速度[m/s^2]
+        x (float): X軸の加速度
+        y (float): Y軸の加速度
+        z (float): Z軸の加速度
 
     Returns:
         float: ピッチ角[rad]
