92f03fdb115a2a5f2a00526e2b7c6ec6b9ee7fe1
shota040927 <23fr054@ms.dendai.ac.jp>
2023-04-28 00:44:16 +0900

    test

--- test/test_battery.py
+++ test/test_battery.py
@@ -1,5 +1,26 @@
-from cansatapi.batteryfuelgauge import BatteryFuelGauge
+import unittest
+from unittest.mock import MagicMock
+from battery_fuel_gauge import BatteryFuelGauge
 
-if __name__ == "__main__":
-    battery = BatteryFuelGauge()
-    print(battery.get_level())
+
+class TestBatteryFuelGauge(unittest.TestCase):
+    # テストクラス
+
+    def test_get_level(self):
+        # get_level()メソッドをテストするためのテストメソッド
+
+        # smbus2ライブラリのモックを作成
+        smbus2 = MagicMock()
+        # モックのread_byte_data()メソッドに返す値を設定
+        smbus2.SMBus.return_value.read_byte_data.return_value = 50
+        # BatteryFuelGaugeクラスのインスタンスを作成し、モックをセット
+        battery = BatteryFuelGauge()
+        battery.bus = smbus2
+
+        # get_level()メソッドを呼び出し、取得した値が想定通りか確認
+        level = battery.get_level()
+        self.assertEqual(level, 50)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- test/test_convert.py
+++ test/test_convert.py
@@ -13,30 +13,33 @@ class TestConvert(unittest.TestCase):
         g = [1, 1, 1]
         m_s2 = 9.80665
         self.assertAlmostEqual(g_to_m_per_s2(g)[0], m_s2)
+
     def raw_ang_rate_to_ang_per_s(self):
-        per_s = 1000
-        absss = 1000
-        self.assertAlmostEqual(raw_ang_rate_to_ang_per_s(per_s, absss), 401)
+        per_s = 1
+        absss = 2000
+        self.assertAlmostEqual(raw_ang_rate_to_ang_per_s(per_s, absss), -250)
+
     def ut_to_azimuth(self):
-        azim = 1.5
-        muth = 0.5
-        self.assertAlmostEqual(ut_to_azimuth(azim, muth), 18.43)
+        azim = 100
+        muth = 100
+        self.assertAlmostEqual(ut_to_azimuth(azim, muth), 45)
+
     def acceleration_to_roll(self):
-        accel = 0.5
-        roll = 1.5
-        self.assertAlmostEqual(acceleration_to_roll(accel, roll), 18.43)
+        accel = 1
+        roll = 1
+        self.assertAlmostEqual(acceleration_to_roll(accel, roll), 0.78)
 
     def acceleration_to_pitch(self):
-        rec = -0.5
-        tec = 1.5
-        xec = 2.5
-        self.assertAlmostEqual(acceleration_to_pitch(rec, tec, xec), -31.93)
+        rec = 1
+        tec = 1
+        xec = 1
+        self.assertAlmostEqual(acceleration_to_pitch(rec, tec, xec), 0.61)
 
     def conv_range(self):
-        xcc = 10
-        xdd = 8
-        xee = 12
-        xff = 10
-        xgg = 15
-        self.assertAlmostEqual(conv_range(xcc, xdd, xee, xff, xgg), 25)
+        xcc = 50
+        xdd = 0
+        xee = 100
+        xff = 0
+        xgg = 1
+        self.assertAlmostEqual(conv_range(xcc, xdd, xee, xff, xgg), 0.5)
 
