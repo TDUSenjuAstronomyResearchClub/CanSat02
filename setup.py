ba681c8b5050ffda733cda8263ffeea7130845b0
Jinnosuke Kato <96569057+jinnosukeKato@users.noreply.github.com>
2023-04-27 21:40:57 +0900

    setup.pyを追加

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+if __name__ == "__name__":
+    setup(
+        name="Basyo CanSat API",
+        version="2.0.0",
+        description="東京電機大学 千住キャンパス 天文学研究部 CanSatプロジェクト Team芭蕉の機体のAPIです"
+    )
