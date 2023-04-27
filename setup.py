5ccf67067b4872c7a8d44a4d5fb17f50d14d610a
Jinnosuke Kato <96569057+jinnosukeKato@users.noreply.github.com>
2023-04-27 21:43:51 +0900

    パッケージにcansatapiが含まれるよう修正

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ if __name__ == "__name__":
     setup(
         name="Basyo CanSat API",
         version="2.0.0",
-        description="東京電機大学 千住キャンパス 天文学研究部 CanSatプロジェクト Team芭蕉の機体のAPIです"
+        description="東京電機大学 千住キャンパス 天文学研究部 CanSatプロジェクト Team芭蕉の機体のAPIです",
+        packages=["cansatapi"]
     )
