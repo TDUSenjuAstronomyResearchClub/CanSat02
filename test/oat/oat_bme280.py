c4c6ab4e3a379306c9fd90418e2ae341a769ea01
Jinnosuke Kato <96569057+jinnosukeKato@users.noreply.github.com>
2023-04-28 10:14:45 +0900

    BME280の動作確認用モジュールを修正

--- test/oat/oat_bme280.py
+++ test/oat/oat_bme280.py
@@ -1,8 +1,17 @@
+import sys
+import time
+
 from cansatapi.temperature import Temperature
 
 if __name__ == "__main__":
     temp = Temperature()
-    result = temp.temperature_result()
-    print(f"温度: {result[0]}")
-    print(f"湿度: {result[1]}")
-    print(f"気圧: {result[2]}")
+    while True:
+        try:
+            result = temp.temperature_result()
+            print(f"温度: {result[0]}")
+            print(f"湿度: {result[1]}")
+            print(f"気圧: {result[2]}")
+            time.sleep(1)
+        except KeyboardInterrupt:
+            sys.exit(0)
+
