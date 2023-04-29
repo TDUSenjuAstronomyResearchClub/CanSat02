3f780dc58c263e2ef4205af54e5bf5db1c496acc
Jinnosuke Kato <96569057+jinnosukeKato@users.noreply.github.com>
2023-04-29 23:25:45 +0900

    いろいろ修正・変更

--- experiment/droping_test.py
+++ experiment/droping_test.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 import time
 import datetime
 import math
@@ -6,82 +7,115 @@ import math
 from cansatapi.barometer import Barometer
 from cansatapi.nineaxissensor import NineAxisSensor
 
-barometer = Barometer()
-nine_axis = NineAxisSensor()
 
+class Logger:
+    """ロガークラス
 
-def log(filename: str, description: str, msg: str):
+    CSV形式でロギングを行います。
     """
 
-    Args:
-        filename (str): ログを書き込むファイル名
-        description (str): ログの説明文
-        msg (str): ログメッセージ
-    """
-    dat_now = datetime.datetime.now()  # 現在日時を取得する
-    f = open(filename, 'a')
-    writer = csv.writer(f, lineterminator='\n')
-    log_list = [dat_now, description, msg]  # リストに各値を挿入
+    def __init__(self, file_name: str):
+        """ロガーのコンストラクタ
 
-    writer.writerow(log_list)
-    print(log_list)
-    f.close()
-    time.sleep(0.1)
-    return
+        CSV形式でロギングを行います。
+
+        Args:
+            file_name: ログファイルの名前
+        """
+        self.file = open(file_name, 'a')
+        self.writer = csv.writer(self.file, lineterminator='\n')
+
+        # CSVにインデックスをつける
+        self.writer.writerow(['現在日時', '説明', '内容'])
+        self.file.close()
+
+    def log(self, description: str, content: str | float):
+        """ロギング用のプライベートメソッド
+
+        Args:
+            description (str): ログの説明文
+            content (str): ログメッセージ
+        """
+        self.file = open(self.file.name, 'a')
+        dt_now = datetime.datetime.now()  # 現在日時を取得する
+        log_list = [dt_now, description, content]  # リストに各値を挿入
+
+        self.writer.writerow(log_list)
+        self.file.close()
+        time.sleep(0.1)
+
+    def msg(self, msg: str):
+        """メッセージロギング用メソッド
+
+        Args:
+            msg (str): メッセージ
+        """
+        self.log("[LOG]", msg)
+        print(msg, file=sys.stdout)
+
+    def error(self, msg: str):
+        """エラーロギング用のメソッド
+
+        Args:
+            msg (str): エラーメッセージ
+        """
+        self.log("[ERROR]", msg)
+        print(msg, file=sys.stderr)
 
 
-# ===ログ用ファイルの作成===
-dt_start = datetime.datetime.now()  # 現在日時を取得する
-filename = 'driving' + dt_start.strftime('%Y年%m月%d日_%H時%M分%S秒') + '.csv'  # ファイル名を現在時刻にする
+if __name__ == "__main__":
+    barometer = Barometer()
+    nine_axis = NineAxisSensor()
 
-f = open(filename, 'a')
-writer = csv.writer(f, lineterminator='\n')
-index = ['Time of Day', 'Log type value', 'value']  # 表のインデックスを付ける
-writer.writerow(index)
-f.close()
+    # ログ用ファイルの作成
+    dt_start = datetime.datetime.now()  # 現在日時を取得する
+    filename = 'driving' + dt_start.strftime('%Y年%m月%d日_%H時%M分%S秒') + '.csv'  # ファイル名を現在時刻にする
+    logger = Logger(filename)
 
-# ===落下開始判定===
-hight_past = -1  # 高度比較用に使用する過去の高度値を初期化する
+    # 落下開始判定
+    altitude_past = -1  # 高度比較用に使用する過去の高度値を初期化する
+    altitude_now = 0
+    while True:
 
-while True:
+        try:
+            d = barometer.get_pressure_altitude_temperature()
+            altitude_now = d[0]  # 現在の高度を求める
+            logger.log("[高度]", altitude_now)  # ログを残す
+        except OSError:
+            logger.error("気圧センサーでOSError")
 
-    try:
-        hight_temp = barometer.get_pressure_altitude_temperature()
-        hight_now = hight_temp[0]  # 現在の高度を求める
-        log(filename, "高度", hight_now)  # ログを残す
-    except OSError:
-        log("気圧センサOSエラー", None)
+        if altitude_past > altitude_now:
+            logger.msg("落下判定が行われました")
+            break
 
-    if hight_past > hight_now:
-        log(filename, "落下判定が行われました", None)
-        break
+        altitude_past = altitude_now
 
-    hight_past = hight_now
+        time.sleep(0.5)
 
-    time.sleep(0.5)
+    # 着地判定
+    altitude = 0
+    accel_abs = 0
+    while True:
 
-# ===着地判定===
-while True:
+        try:
+            altitude = barometer.get_pressure_altitude_temperature()[1]
+            logger.log("[高度]", altitude)  # ログを残す
+        except OSError:
+            logger.error("気圧センサでOSError")
 
-    try:
-        hight = barometer.get_pressure_altitude_temperature()
-        log(filename, "高度", hight[0])  # ログを残す
-    except OSError:
-        log("気圧センサOSエラー", None)
+        try:
+            accel = nine_axis.get_acceleration()
+            accel_abs = math.sqrt(accel[0] ** 2 + accel[1] ** 2 + accel[2] ** 2)  # 9軸から加速度の大きさを求める
+            logger.log("[加速度]", accel_abs)  # ログを残す
+        except OSError:
+            logger.error("9軸センサでOSError")
 
-    try:
-        accel_tmp = nine_axis.get_acceleration()
-        accel = math.sqrt(accel_tmp[0] ^ 2 + accel_tmp[1] ^ 2 + accel_tmp[2] ^ 2)  # 9軸から加速度の大きさを求める
-        log(filename, "加速度の大きさ", accel)  # ログを残す
-    except OSError:
-        log("9軸センサOSエラー", None)
+        # 高度と加速度の着地判定の基準値を書き込む
+        if altitude < 0 or accel_abs < 0:
+            logger.msg("着地判定が行われました")
+            break
 
-    # ===高度と加速度の着地判定の基準値を書き込む===
-    if hight[0] < 0 or accel < 0:
-        log(filename, "着地判定が行われました", None)
-        break
+        time.sleep(0.5)
 
-    time.sleep(0.5)
-
-# ===DCモータでパラシュートを分離させる===
+# DCモータでパラシュートを分離させる
 # プログラム作成の必要あり
