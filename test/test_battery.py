0a195381dd1e1237bc37550068abba6e8ecbf557
shota040927 <23fr054@ms.dendai.ac.jp>
2023-05-03 22:23:40 +0900

    モータの挙動指定

--- test/test_battery.py
+++ test/test_battery.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import MagicMock
-from battery_fuel_gauge import BatteryFuelGauge
+from batteryfuelgauge import BatteryFuelGauge
 
 
 class TestBatteryFuelGauge(unittest.TestCase):
