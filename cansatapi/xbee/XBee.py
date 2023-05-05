aa9a284cd31123719077b59c9af2e5198e9aefd7
Jinnosuke Kato <96569057+jinnosukeKato@users.noreply.github.com>
2023-05-06 08:50:03 +0900

    例外処理周りを書き換え

--- cansatapi/xbee/XBee.py
+++ cansatapi/xbee/XBee.py
@@ -1,11 +1,13 @@
 """機体と地上局の通信を行うモジュール
 """
 
+import datetime
 import json
 import time
-import datetime
+
 import serial
 from serial import SerialException
+from serial import PortNotOpenError
 
 # ポート設定
 PORT = '/dev/ttyUSB0'
@@ -22,6 +24,10 @@ def send(filename: str, json_data: str):
     Args:
         filename (str): ファイルネーム
         json_data (str): JSON形式のデータ
+
+    Raises:
+        PortNotOpenError: ポートが空いておらず、リトライにも失敗した場合発生します
+        SerialException: デバイスがみつからないときに発生します
     """
     # ログ用ファイルをオープン
     f = open(filename, 'a')
@@ -29,29 +35,25 @@ def send(filename: str, json_data: str):
     json.dump(json_data, f, indent=4, ensure_ascii=False)
     f.close()
 
+    retry_c = 0
     while True:
-        global is_using_serial_port
-        if not is_using_serial_port:  # シリアルポートは使用中ではないか
-            # シリアルポート使用判定フラグを使用中にする
-            is_using_serial_port = True
+        try:
+            ser = serial.Serial(PORT, BAUD_RATE)
+            # シリアルにjsonを書き込む
+            ser.write(json_data.encode('utf-8'))
+            ser.close()
+            return
 
-            try:
-                ser = serial.Serial(PORT, BAUD_RATE)
-                # シリアルにjsonを書き込む
-                ser.write(json_data.encode('utf-8'))
-                ser.close()
-
-                is_using_serial_port = False
-                break
-
-            except SerialException:  # デバイスが見つからない、または構成できない場合
-                is_using_serial_port = False
-                break  # ここの処理について要件等
-
-        else:
-            time.sleep(0.5)
-
-    return
+        except PortNotOpenError:
+            # 5回リトライに失敗したらエラーを吐く
+            retry_c += 1
+            if retry_c > 5:
+                raise PortNotOpenError
+            else:
+                time.sleep(0.5)
+                continue
+        except SerialException:
+            raise SerialException  # ここの処理について要件等
 
 
 def receive(filename: str) -> str:
@@ -64,33 +66,31 @@ def receive(filename: str) -> str:
         SerialException: デバイスがみつからないときに発生します
         PortNotOpenError: ポートが空いていないときに発生します
     """
-    global is_using_serial_port
-    if not is_using_serial_port:
-        # シリアルポート使用判定フラグを使用中にする
-        is_using_serial_port = True
-
+    retry_c = 0
+    while True:
         try:
-            now = datetime.datetime.now()  # センサ値取得開始日時の取得
             ser = serial.Serial(PORT, BAUD_RATE, timeout=0.1)
-            utf8_string = ser.read_all()  # 機体から値を受け取る
+            receive_data = ser.read_all()  # 機体から値を受け取る
             ser.close()
 
             catch_value = {
-                "time": now,
-                "message": utf8_string
+                "time": datetime.datetime.now(),
+                "message": receive_data
             }
             # ログ用ファイルをオープン
             f = open(filename, 'a')
             # jsonとして書き込み
             json.dump(catch_value, f, indent=4, ensure_ascii=False)
             f.close()
+            return receive_data
 
-            is_using_serial_port = False
-            return utf8_string
-
+        except PortNotOpenError:
+            # 5回リトライに失敗したらエラーを吐く
+            retry_c += 1
+            if retry_c > 5:
+                raise PortNotOpenError
+            else:
+                time.sleep(0.5)
+                continue
         except SerialException:  # デバイスが見つからない、または構成できない場合
-            is_using_serial_port = False
             raise SerialException
-
-    else:
-        raise serial.PortNotOpenError
