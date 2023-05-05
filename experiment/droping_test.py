b05ec40936406a47f25cb9b13ae753d5b42786d2
Jinnosuke Kato <96569057+jinnosukeKato@users.noreply.github.com>
2023-05-05 16:24:30 +0900

    ロガーをライブラリに移管

--- experiment/droping_test.py
+++ experiment/droping_test.py
@@ -1,7 +1,5 @@
 """2023/5/6の投下試験用のテストモジュール
 """
-import csv
-import sys
 import time
 import datetime
 import math
@@ -9,61 +7,7 @@ import math
 from cansatapi.lps25hb import LPS25HB
 from cansatapi.lps25hb import calc_altitude
 from cansatapi.nineaxissensor import NineAxisSensor
-
-
-class Logger:
-    """ロガークラス
-
-    CSV形式でロギングを行います。
-    """
-
-    def __init__(self, file_name: str):
-        """ロガーのコンストラクタ
-
-        CSV形式でロギングを行います。
-
-        Args:
-            file_name: ログファイルの名前
-        """
-        self.file = open(file_name, 'a')
-        self.writer = csv.writer(self.file, lineterminator='\n')
-
-        # CSVにインデックスをつける
-        self.writer.writerow(['現在日時', '説明', '内容'])
-        self.file.close()
-
-    def log(self, category: str, content: str | float):
-        """ロギング用のメソッド
-
-        Args:
-            category (str): ログのカテゴリ
-            content (str): ログメッセージ
-        """
-        self.file = open(self.file.name, 'a')
-        dt_now = datetime.datetime.now()  # 現在日時を取得する
-        log_list = [dt_now, category, content]  # リストに各値を挿入
-
-        self.writer.writerow(log_list)
-        self.file.close()
-        time.sleep(0.1)
-
-    def msg(self, msg: str):
-        """メッセージロギング用メソッド
-
-        Args:
-            msg (str): メッセージ
-        """
-        self.log("message", msg)
-        print("[Msg]" + msg, file=sys.stdout)
-
-    def error(self, msg: str):
-        """エラーロギング用のメソッド
-
-        Args:
-            msg (str): エラーメッセージ
-        """
-        self.log("ERROR", msg)
-        print("[Error]" + msg, file=sys.stderr)
+from cansatapi.util.logger import Logger
 
 
 if __name__ == "__main__":
