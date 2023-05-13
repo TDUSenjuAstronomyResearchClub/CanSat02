54d6724cae3d0c88fd1e5ac7345d8384e4afb317
jinnosukeKato <jinnosukeKato@gmail.com>
2023-05-13 14:57:16 +0900

    送信するメッセージデータを定数を参照する形に変更

--- operation_check/oc_xbee_send.py
+++ operation_check/oc_xbee_send.py
@@ -2,11 +2,14 @@ import sys
 import time
 
 from cansatapi import XBee
+
+MSG = "test"
+
 if __name__ == "__main__":
     while True:
         try:
-            XBee.send("test")
-            print("Send message")
+            XBee.send(MSG)
+            print(f"Send message: {MSG}")
             time.sleep(1)
         except KeyboardInterrupt:
             sys.exit(0)
