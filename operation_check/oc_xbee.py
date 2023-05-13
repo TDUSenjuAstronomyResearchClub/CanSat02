af16e99afe2919be59f4a21c5c26be5d8a58763d
jinnosukeKato <jinnosukeKato@gmail.com>
2023-05-13 14:28:16 +0900

    xbeeの動確モジュールを追加

---
+++ operation_check/oc_xbee.py
@@ -0,0 +1,12 @@
+import sys
+import time
+
+from cansatapi.xbee import XBee
+if __name__ == "__main__":
+    while True:
+        try:
+            XBee.send("test")
+            print("Send message")
+            time.sleep(1000)
+        except KeyboardInterrupt:
+            sys.exit(0)
