f00b435489b09dec87e1e20961961813ee195e69
nakanishi <23fr083@ms.dendai.ac.jp>
2023-05-19 00:22:33 +0900

    後退・右旋回・左旋回の関数作成

--- running/geared_motor.py
+++ running/geared_motor.py
@@ -21,3 +21,55 @@ def StraightLine():
     motor_r.stop()
     motor_l.stop()
     time.sleep(1)  # 1秒間待つ
+
+def Back():
+    """3秒間後退させるための関数
+    """
+
+    # DCモータのインスタンスを生成
+    motor_r = DCMotor(6, 5)
+    motor_l = DCMotor(26, 19)
+
+    # それぞれのモーターのデューティ比を80%に設定
+    motor_r.forward(80)
+    motor_l.forward(80)
+    time.sleep(3)  # 3秒間後退
+
+    # それぞれのモーターを停止
+    motor_r.stop()
+    motor_l.stop()
+    time.sleep(1)  # 1秒間待つ
+
+def TurnRight():
+    """右旋回させるための関数
+    """
+
+    # DCモータのインスタンスを生成
+    motor_r = DCMotor(6, 5)
+    motor_l = DCMotor(26, 19)
+
+    # 右のモーターのデューティ比を80%に設定
+    motor_r.forward(80)
+    motor_l.forward()
+
+    # 右のモーターを停止
+    motor_r.stop()
+    motor_l.stop()
+    time.sleep(1)  # 1秒間待つ
+
+def TurnLeft():
+    """左旋回させるための関数
+    """
+
+    # DCモータのインスタンスを生成
+    motor_r = DCMotor(6, 5)
+    motor_l = DCMotor(26, 19)
+
+    # 左のモーターのデューティ比を80%に設定
+    motor_r.forward()
+    motor_l.forward(80)
+
+    # 左のモーターを停止
+    motor_r.stop()
+    motor_l.stop()
+    time.sleep(1)  # 1秒間待つ
