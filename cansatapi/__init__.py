dc57b36052289f3a10d27ffee7f6b899b3dd2d96
jinnosukeKato <jinnosukeKato@gmail.com>
2023-05-18 16:24:42 +0900

    __all__を定義

---
+++ cansatapi/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "batteryfuelgauge",
+    "bme280",
+    "camera",
+    "dcmotor",
+    "distance",
+    "gps",
+    "lps25hb",
+    "nineaxissensor",
+    "servo",
+    "soil_moisture",
+    "XBee"
+]
