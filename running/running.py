c4a3bc8ee71462dec27344f8a45621aac3843bb8
jinnosukeKato <jinnosukeKato@gmail.com>
2023-05-18 16:33:33 +0900

    パラシュート切り離しまでを作成

--- running/running.py
+++ running/running.py
@@ -1,4 +1,19 @@
+import datetime
+import time
+
+from cansatapi.util import logger
+from cansatapi.util.logger import Logger
 from cansatapi.gps import get_gps_data
+from cansatapi import *
+
+# 本番前に記入
+SAMPLE_LON: float = 0.0
+SAMPLE_LAT: float = 0.0
+
+GOAL_LON: float = 0.0
+GOAL_LAT: float = 0.0
+
+
 # gps.pyからサンプル取得地点の緯度経度値を取得する関数
 
 
@@ -10,6 +25,7 @@ def get_sample():
     sample_latitude = 35.6789
     sample_longitude = 139.0123
     return sample_latitude, sample_longitude
+
 
 # SeeValueに渡す緯度経度値を含む関数
 
@@ -33,6 +49,7 @@ def SeeValue():
     # ゴールの緯度経度、サンプル取得地点の緯度経度の順で返す
     return goal_latitude, goal_longitude, sample_latitude, sample_longitude
 
+
 # 関数を呼び出して緯度経度値を表示
 
 
@@ -44,3 +61,45 @@ goal_latitude, goal_longitude, sample_latitude, sample_longitude = result
 # ここで結果を使った処理を行う
 print("ゴール地点の緯度経度値:", goal_latitude, goal_longitude)
 print("サンプル取得地点の緯度経度値:", sample_latitude, sample_longitude)
+
+
+def FallJudgement():
+    """落下判定をするまで待つ関数
+    """
+
+
+def LandingJudgement():
+    """着地判定をするまで待つ関数"""
+
+
+def detach_parachute(logger: Logger):
+    """パラシュートの切り離しを行います
+    """
+    para_motor = dcmotor.DCMotor(9, 10)
+    logger.msg("パラシュート切り離し開始")
+    para_motor.forward()
+
+    time.sleep(10)  # 10秒間巻取り
+
+    para_motor.stop()
+    para_motor.cleanup()
+    logger.msg("パラシュート切り離し終了")
+
+
+def main():
+    """メインアルゴリズム
+    """
+    main_logger = Logger("Running" + datetime.datetime.now().strftime(logger.DATETIME_F))
+    XBee.send_msg("Start running")
+
+    FallJudgement()
+    XBee.send_msg("Detect falling")
+
+    LandingJudgement()
+    XBee.send_msg("Landed")
+
+    detach_parachute(main_logger)
+
+
+if __name__ == "__main__":
+    main()  # 実行
