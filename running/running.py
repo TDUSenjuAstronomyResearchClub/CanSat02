137cdd9539c4a6bebead21e05f8071dd292f32a8
jinnosukeKato <jinnosukeKato@gmail.com>
2023-05-22 14:57:38 +0900

    スペルミスを修正

--- running/running.py
+++ running/running.py
@@ -15,14 +15,7 @@ GOAL_LON: float = 0.0
 GOAL_LAT: float = 0.0
 
 # 共有変数
-recive_command = ''
-
-def recive_data():
-    """XBeeからデータを受信する関数(別スレッドで動作)
-    """
-    global recive_command
-    while True:
-        recive_command = XBee.receive()
+receive_command = ''
 
 def manual_mode():
     """手動制御を行う関数
@@ -31,15 +24,15 @@ def manual_mode():
 def FallJudgement():
     """落下判定をするまで待つ関数
     """
-    global recive_command
+    global receive_command
     while True:
-        if recive_command == 'manual':
+        if receive_command == 'manual':
             #TODO: マニュアルモードの関数を呼び出す
-            recive_command = '' #コマンドを初期化
+            receive_command = '' #コマンドを初期化
 
-        elif recive_command == 'distination':
+        elif receive_command == 'distination':
             #TODO: 目的地の緯度経度を変更する関数を呼び出す
-            recive_command = '' #コマンドを初期化
+            receive_command = '' #コマンドを初期化
 
         else:
             #TODO: 落下開始判定をするプログラムを書く（drop_test.pyを参考に）
@@ -51,17 +44,17 @@ def FallJudgement():
 def LandingJudgement():
     """着地判定をするまで待つ関数
     """
-    global recive_command
+    global receive_command
     while True:
-        if recive_command == 'manual':
+        if receive_command == 'manual':
             #TODO: マニュアルモードの関数を呼び出す
-            recive_command = '' #コマンドを初期化
+            receive_command = '' #コマンドを初期化
 
-        elif recive_command == 'distination':
+        elif receive_command == 'destination':
             #TODO: 目的地の緯度経度を変更する関数を呼び出す
-            recive_command = '' #コマンドを初期化
+            receive_command = '' #コマンドを初期化
         
-        elif recive_command == 'landing':   #着地判定を手動で行う
+        elif receive_command == 'landing':   #着地判定を手動で行う
             return
             
         else:
@@ -106,9 +99,9 @@ def main():
     """メインアルゴリズム
     """
     # Xbeeからのデータ受信用スレッド作成
-    recive_data = threading.Thread(target=recive_data)
+    receive_data = threading.Thread(target=XBee.receive)
     # Xbeeからのデータ受信用スレッドをスタート
-    recive_data.start()
+    receive_data.start()
 
     main_logger = Logger("Running" + datetime.datetime.now().strftime(logging.DATETIME_F))
     XBee.send_msg("Start running")
