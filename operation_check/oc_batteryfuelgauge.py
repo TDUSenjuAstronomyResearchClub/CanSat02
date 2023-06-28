82b6ac6192c35acf3644979013723e8d263de46c
taichi <21ec109@ms.dendai.ac.jp>
2023-06-28 21:30:56 +0900

    モジュール名を変更

--- operation_check/oc_batteryfuelgauge.py
+++ operation_check/oc_batteryfuelgauge.py
@@ -1,12 +1,12 @@
 """電流センサの動作確認用モジュール"""
 import sys
 import time
-from cansatapi import batteryfuelgauge
+from cansatapi import batteryvoltagegauge
 
 if __name__ == 'main':
     while True:
         try:
-            print(f"バッテリー電圧{batteryfuelgauge.get_level()}[%]")
+            print(f"バッテリー電圧{batteryvoltagegauge.get_level()}[%]")
             time.sleep(1)
 
         except KeyboardInterrupt:
