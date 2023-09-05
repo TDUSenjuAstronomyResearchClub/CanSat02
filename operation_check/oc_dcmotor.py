2e4f63165a931ad780c8897d23827ed3517bad2d
Else156 <21ec109@ms.dendai.ac.jp>
2023-09-05 16:13:58 +0900

    動作確認用の処理内容出力用のprin文追加

--- operation_check/oc_dcmotor.py
+++ operation_check/oc_dcmotor.py
@@ -4,18 +4,24 @@ from cansatapi import dcmotor
 
 if __name__ == "__main__":
     dcmotor.Wheels.stop()
+    print("wheels stop")
     dcmotor.Wheels.forward()
+    print("wheels forward")
     time.sleep(2)
 
     dcmotor.Wheels.reverse()
+    print("wheels reverse")
     time.sleep(2)
 
     para = dcmotor.DCMotor(dcmotor.PARACHUTE_FIN, dcmotor.PARACHUTE_RIN)
     para.forward()
+    print("para motor forward")
     time.sleep(2)
     para.reverse()
+    print("para motor reverse")
     time.sleep(2)
     para.stop()
+    print("para motor stop")
     para.cleanup()
 
     dcmotor.Wheels.cleanup()
