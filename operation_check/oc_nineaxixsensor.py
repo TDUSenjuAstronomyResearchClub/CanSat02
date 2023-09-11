cfc97e8864bfe9010848c12966e937e3e42a7960
Else156 <21ec109@ms.dendai.ac.jp>
2023-09-11 17:20:04 +0900

    print文のフォーマットを合わせた

--- operation_check/oc_nineaxixsensor.py
+++ operation_check/oc_nineaxixsensor.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         try:
             acc = nineaxis.get_acceleration()
             accel_abs = math.sqrt(acc[0] ** 2 + acc[1] ** 2 + acc[2] ** 2)  # 9軸から加速度の大きさを求める
-            print(f'加速度 X: {acc[0]}, Y: {acc[1]}, Z: {acc[2]},絶対値{accel_abs}')
+            print(f'加速度 X: {acc[0]}, Y: {acc[1]}, Z: {acc[2]}, 絶対値: {accel_abs}')
 
             ang_rate = nineaxis.get_angular_rate()
             print(f'角速度 X: {ang_rate[0]}, Y: {ang_rate[1]}, Z: {ang_rate[2]}')
