050a4beffec0c1921452bd1123c20f4e5d91fb01
Else156 <21ec109@ms.dendai.ac.jp>
2023-09-11 18:36:46 +0900

    パラシュート分離用モーターのプログラム削除

--- operation_check/oc_dcmotor.py
+++ operation_check/oc_dcmotor.py
@@ -10,12 +10,4 @@ if __name__ == "__main__":
     dcmotor.Wheels.reverse()
     time.sleep(2)
 
-    para = dcmotor.DCMotor(dcmotor.PARACHUTE_FIN, dcmotor.PARACHUTE_RIN)
-    para.forward()
-    time.sleep(2)
-    para.reverse()
-    time.sleep(2)
-    para.stop()
-    para.cleanup()
-
     dcmotor.Wheels.cleanup()
