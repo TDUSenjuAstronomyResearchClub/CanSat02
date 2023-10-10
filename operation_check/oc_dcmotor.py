ca7034be600d4b095f6b91f2e689221184d50b0a
Else156 <21ec109@ms.dendai.ac.jp>
2023-10-10 18:45:33 +0900

    デバック用print文を追加

--- operation_check/oc_dcmotor.py
+++ operation_check/oc_dcmotor.py
@@ -4,10 +4,13 @@ from cansatapi import dcmotor
 import RPi.GPIO as GPIO
 
 if __name__ == "__main__":
+    print("デバック:モーターストップ")
     dcmotor.Wheels.stop()
+    print("デバック:モーター前進")
     dcmotor.Wheels.forward()
     time.sleep(2)
 
+    print("デバック:モーター後退")
     dcmotor.Wheels.reverse()
     time.sleep(2)
 
