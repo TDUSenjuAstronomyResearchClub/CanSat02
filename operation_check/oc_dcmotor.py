43c9acffb97aee3a4329fc549c75028160c3335a
Else156 <21ec109@ms.dendai.ac.jp>
2023-10-13 16:42:08 +0900

    delayをくそでかにした（テスト用）

--- operation_check/oc_dcmotor.py
+++ operation_check/oc_dcmotor.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     dcmotor.Wheels.stop()
     print("デバック:モーター前進")
     dcmotor.Wheels.forward()
-    time.sleep(2)
+    time.sleep(20000000000000000)
 
     print("デバック:モーター後退")
     dcmotor.Wheels.reverse()
