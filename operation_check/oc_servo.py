fbd4b8c8457e062d204686303395c9df38791a44
Else156 <21ec109@ms.dendai.ac.jp>
2023-10-30 19:51:27 +0900

    ローテーションサーボモーターに対応させる

--- operation_check/oc_servo.py
+++ operation_check/oc_servo.py
@@ -11,48 +11,52 @@ soil_servo = Servo(25)
 # パラシュート分離用モーターにパラシュート分離の動作をさせる
 try:
     for _ in range(20):
-        para_servo.rotate_to_angle(90)
+        para_servo.rotate_cw_or_ccw(3.5)
         time.sleep(2)
 except KeyboardInterrupt:
-    para_servo.rotate_to_angle(0)
+    para_servo.rotate_cw_or_ccw(7.5)
+    para_servo.stop()
 
+para_servo.stop()
 time.sleep(1)
 
 # サンプル採取用モーターにサンプル採取機構を土と接触させる動作をさせる
 print("サンプル採取機構を土と接触させる")
 try:
     for _ in range(10):
-        sample_servo.rotate_to_angle(90)
+        sample_servo.rotate_cw_or_ccw(3.5)
         time.sleep(2)
 except KeyboardInterrupt:
-    sample_servo.rotate_to_angle(0)
+    sample_servo.rotate_cw_or_ccw(7.5)
 
 # サンプル採取機構を機体に格納する
 print("サンプル採取機構を機体に格納する")
 try:
     for _ in range(10):
-        sample_servo.rotate_to_angle(-90)
+        sample_servo.rotate_cw_or_ccw(11.5)
         time.sleep(2)
 except KeyboardInterrupt:
-    sample_servo.rotate_to_angle(0)
+    sample_servo.rotate_cw_or_ccw(7.5)
 
+sample_servo.stop()
 time.sleep(1)
 
 # 土壌水分量測定モータで土壌水分センサを土に挿し込む
 print("土壌水分センサを土に挿し込む")
 try:
     for _ in range(10):
-        soil_servo.rotate_to_angle(90)
+        soil_servo.rotate_cw_or_ccw(3.5)
         time.sleep(2)
 except KeyboardInterrupt:
-    soil_servo.rotate_to_angle(0)
+    soil_servo.rotate_cw_or_ccw(7.5)
 
 # 土壌水分センサを機体に格納する
 print("土壌水分センサを機体に格納する")
 try:
     for _ in range(10):
-        soil_servo.rotate_to_angle(-90)
+        soil_servo.rotate_cw_or_ccw(11.5)
         time.sleep(2)
 except KeyboardInterrupt:
-    soil_servo.rotate_to_angle(0)
+    soil_servo.rotate_cw_or_ccw(7.5)
 
+soil_servo.stop()
