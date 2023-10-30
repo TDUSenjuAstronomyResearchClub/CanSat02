15b14826fb98cd0ad9cb49f1c2d475572badfe79
Else156 <21ec109@ms.dendai.ac.jp>
2023-10-30 13:03:22 +0900

    各サーボモーターの役割に伴った動作をさせるよう変更

--- operation_check/oc_servo.py
+++ operation_check/oc_servo.py
@@ -1,39 +1,73 @@
 """サーボモーターの動作確認用モジュール
+各サーボモーターの役割に伴った動作をさせる
 """
 from cansatapi.servo import Servo
-import sys
 import time
 
-if __name__ == "__main__":
-    servo1 = Servo(23)
-    servo2 = Servo(24)
-    servo3 = Servo(25)
+para_servo = Servo(23)
+sample_servo = Servo(24)
+soil_servo = Servo(25)
 
+# パラシュート分離用モーターにパラシュート分離の動作をさせる
+para_start_time = time.time()
+try:
     while True:
-        try:
-            print("servo1")
-            servo1.rotate_to_angle(90)
-            servo1.rotate_to_angle(0)
-            servo1.rotate_to_angle(-90)
-            servo1.rotate_to_angle(0)
-            time.sleep(1)
+        para_servo.rotate_to_angle(90)
+        if time.time() - para_start_time >= 20:
+            para_servo.rotate_to_angle(0)
+            break
+except KeyboardInterrupt:
+    para_servo.rotate_to_angle(0)
 
-            print("servo2")
-            servo2.rotate_to_angle(90)
-            servo2.rotate_to_angle(0)
-            servo2.rotate_to_angle(-90)
-            servo2.rotate_to_angle(0)
-            time.sleep(1)
+time.sleep(1)
 
-            print("servo3")
-            servo3.rotate_to_angle(90)
-            servo3.rotate_to_angle(0)
-            servo3.rotate_to_angle(-90)
-            servo3.rotate_to_angle(0)
-            time.sleep(1)
+# サンプル採取用モーターにサンプル採取機構を土と接触させる動作をさせる
+print("サンプル採取機構を土と接触させる")
+sample_start_time = time.time()
+try:
+    while True:
+        sample_servo.rotate_to_angle(90)
+        if time.time() - sample_start_time >= 5:
+            sample_servo.rotate_to_angle(0)
+            break
+except KeyboardInterrupt:
+    sample_servo.rotate_to_angle(0)
 
-        except KeyboardInterrupt:
-            servo1.stop()
-            servo2.stop()
-            servo3.stop()
-            sys.exit(0)
+# サンプル採取機構を機体に格納する
+print("サンプル採取機構を機体に格納する")
+start_pull_out_time = time.time()
+try:
+    while True:
+        sample_servo.rotate_to_angle(-90)
+        if time.time() - start_pull_out_time >= 10:
+            sample_servo.rotate_to_angle(0)
+            break
+except KeyboardInterrupt:
+    sample_servo.rotate_to_angle(0)
+
+time.sleep(1)
+
+# 土壌水分量測定モータで土壌水分センサを土に挿し込む
+print("土壌水分センサを土に挿し込む")
+start_insert_time = time.time()
+try:
+    while True:
+        soil_servo.rotate_to_angle(90)
+        if time.time() - start_insert_time >= 10:
+            soil_servo.rotate_to_angle(0)
+            break
+except KeyboardInterrupt:
+    soil_servo.rotate_to_angle(0)
+
+# 土壌水分センサを機体に格納する
+print("土壌水分センサを機体に格納する")
+start_pull_out_time = time.time()
+try:
+    while True:
+        soil_servo.rotate_to_angle(-90)
+        if time.time() - start_pull_out_time >= 10:
+            soil_servo.rotate_to_angle(0)
+            break
+except KeyboardInterrupt:
+    soil_servo.rotate_to_angle(0)
+
