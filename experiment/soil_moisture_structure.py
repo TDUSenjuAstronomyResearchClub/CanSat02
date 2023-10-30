61550c05eb77c23aaba93f77d04460d64047dde1
Else156 <21ec109@ms.dendai.ac.jp>
2023-10-30 12:04:36 +0900

    変数名の修正

--- experiment/soil_moisture_structure.py
+++ experiment/soil_moisture_structure.py
@@ -6,31 +6,31 @@ from cansatapi.soil_moisture import SoilMoistureSensor
 import time
 
 # パラシュート分離用のgpioを指定
-para_servo = Servo(25)
+soil_servo = Servo(25)
 
 sensor = SoilMoistureSensor()
 start_insert_time = time.time()
 
 try:
     while True:
-        para_servo.rotate_to_angle(90)
+        soil_servo.rotate_to_angle(90)
         # TODO: 土壌水分センサが土に挿入するのにかかる時間を測定し，変更・反映させる
         if time.time() - start_insert_time >= 10:
-            para_servo.rotate_to_angle(0)
+            soil_servo.rotate_to_angle(0)
             break
 except KeyboardInterrupt:
-    para_servo.rotate_to_angle(0)
+    soil_servo.rotate_to_angle(0)
 
 print(f"{sensor.get_soil_moisture()}")
 
 start_pull_out_time = time.time()
 try:
     while True:
-        para_servo.rotate_to_angle(-90)
+        soil_servo.rotate_to_angle(-90)
         # TODO: 土壌水分センサが機体に格納されるのにかかる時間を測定し，変更・反映させる
         if time.time() - start_pull_out_time >= 10:
-            para_servo.rotate_to_angle(0)
+            soil_servo.rotate_to_angle(0)
             break
 except KeyboardInterrupt:
-    para_servo.rotate_to_angle(0)
+    soil_servo.rotate_to_angle(0)
 
