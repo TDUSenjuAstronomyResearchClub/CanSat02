9b306dd9e42b14f303535f34e49753dbaa9cf380
Else156 <21ec109@ms.dendai.ac.jp>
2023-10-30 14:11:34 +0900

    サーボモーターを複数回数回せるようにした

--- operation_check/oc_servo.py
+++ operation_check/oc_servo.py
@@ -9,13 +9,12 @@ sample_servo = Servo(24)
 soil_servo = Servo(25)
 
 # パラシュート分離用モーターにパラシュート分離の動作をさせる
-para_start_time = time.time()
 try:
-    while True:
-        para_servo.rotate_to_angle(90)
-        if time.time() - para_start_time >= 20:
-            para_servo.rotate_to_angle(0)
-            break
+    for _ in range(20):
+        para_servo.rotate_to_angle(180)
+        time.sleep(0.1)
+        para_servo.rotate_to_angle(0)
+        time.sleep(0.1)
 except KeyboardInterrupt:
     para_servo.rotate_to_angle(0)
 
@@ -23,51 +22,47 @@ time.sleep(1)
 
 # サンプル採取用モーターにサンプル採取機構を土と接触させる動作をさせる
 print("サンプル採取機構を土と接触させる")
-sample_start_time = time.time()
 try:
-    while True:
-        sample_servo.rotate_to_angle(90)
-        if time.time() - sample_start_time >= 5:
-            sample_servo.rotate_to_angle(0)
-            break
+    for _ in range(10):
+        sample_servo.rotate_to_angle(180)
+        time.sleep(0.1)
+        sample_servo.rotate_to_angle(0)
+        time.sleep(0.1)
 except KeyboardInterrupt:
     sample_servo.rotate_to_angle(0)
 
 # サンプル採取機構を機体に格納する
 print("サンプル採取機構を機体に格納する")
-start_pull_out_time = time.time()
 try:
-    while True:
-        sample_servo.rotate_to_angle(-90)
-        if time.time() - start_pull_out_time >= 10:
-            sample_servo.rotate_to_angle(0)
-            break
+    for _ in range(10):
+        sample_servo.rotate_to_angle(-180)
+        time.sleep(0.1)
+        sample_servo.rotate_to_angle(-0)
+        time.sleep(0.1)
 except KeyboardInterrupt:
-    sample_servo.rotate_to_angle(0)
+    sample_servo.rotate_to_angle(-0)
 
 time.sleep(1)
 
 # 土壌水分量測定モータで土壌水分センサを土に挿し込む
 print("土壌水分センサを土に挿し込む")
-start_insert_time = time.time()
 try:
-    while True:
-        soil_servo.rotate_to_angle(90)
-        if time.time() - start_insert_time >= 10:
-            soil_servo.rotate_to_angle(0)
-            break
+    for _ in range(10):
+        soil_servo.rotate_to_angle(180)
+        time.sleep(0.1)
+        soil_servo.rotate_to_angle(0)
+        time.sleep(0.1)
 except KeyboardInterrupt:
     soil_servo.rotate_to_angle(0)
 
 # 土壌水分センサを機体に格納する
 print("土壌水分センサを機体に格納する")
-start_pull_out_time = time.time()
 try:
-    while True:
-        soil_servo.rotate_to_angle(-90)
-        if time.time() - start_pull_out_time >= 10:
-            soil_servo.rotate_to_angle(0)
-            break
+    for _ in range(10):
+        soil_servo.rotate_to_angle(-180)
+        time.sleep(0.1)
+        soil_servo.rotate_to_angle(-0)
+        time.sleep(0.1)
 except KeyboardInterrupt:
     soil_servo.rotate_to_angle(0)
 
