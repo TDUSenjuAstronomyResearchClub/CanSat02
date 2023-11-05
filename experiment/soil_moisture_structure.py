b9431f86f77fdcf79e8ddfaf78f78c5a644aa6dd
Else156 <21ec109@ms.dendai.ac.jp>
2023-11-05 15:02:34 +0900

    土壌水分量測定機構：servo.pyの変更に対応させる

--- experiment/soil_moisture_structure.py
+++ experiment/soil_moisture_structure.py
@@ -8,30 +8,27 @@ import time
 # 土壌水分センサ差し込み用モータのpin番号を指定
 soil_motor_pin = 25
 
-
 # サーボモーターと土壌水分センサのインスタンスを宣言
 soil_servo = Servo(soil_motor_pin)
 sensor = SoilMoistureSensor()
+
 try:
     print("土壌水分センサを土に挿し込む")
-    soil_servo.rotate_cw_or_ccw(11.5)
+    soil_servo.rotate_ccw()
     time.sleep(0.5)
-    soil_servo.stop()
+    soil_servo.rotate_stop()
 except KeyboardInterrupt:
-    soil_servo.stop()
-
+    soil_servo.finish()
 
 # 土壌水分量を出力
 print(f"{sensor.get_soil_moisture()}")
 
-
 # 土壌水分センサを機体に格納する
-soil_servo = Servo(soil_motor_pin)
 try:
     print("土壌水分センサを機体に格納する")
-    soil_servo.rotate_cw_or_ccw(3.5)
+    soil_servo.rotate_cw()
     time.sleep(0.5)
-    soil_servo.stop()
+    soil_servo.finish()
 except KeyboardInterrupt:
-    soil_servo.stop()
+    soil_servo.finish()
 
