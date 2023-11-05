a48f5e7708f72965d6dfb8c23a8c04644cb4b58a
Else156 <21ec109@ms.dendai.ac.jp>
2023-11-05 13:38:44 +0900

    インスタンスの宣言位置を変更

--- operation_check/oc_servo.py
+++ operation_check/oc_servo.py
@@ -4,9 +4,10 @@
 from cansatapi.servo import Servo
 import time
 
-para_servo = Servo(23)
+
 # パラシュート分離用モーターにパラシュート分離の動作をさせる
 print("パラシュート分離用モーターを動かす")
+para_servo = Servo(23)
 try:
     para_servo.rotate_cw_or_ccw(3.5)
     time.sleep(10)
@@ -16,9 +17,10 @@ except KeyboardInterrupt:
 para_servo.stop()
 time.sleep(1)
 
-sample_servo = Servo(24)
+
 # サンプル採取用モーターにサンプル採取機構を土と接触させる動作をさせる
 print("サンプル採取機構を土と接触させる")
+sample_servo = Servo(24)
 try:
     sample_servo.rotate_cw_or_ccw(11.5)
     time.sleep(0.3)
@@ -29,6 +31,7 @@ time.sleep(1)
 
 # サンプル採取機構を機体に格納する
 print("サンプル採取機構を機体に格納する")
+sample_servo = Servo(24)
 try:
     sample_servo.rotate_cw_or_ccw(3.5)
     time.sleep(0.3)
@@ -37,9 +40,10 @@ except KeyboardInterrupt:
 sample_servo.stop()
 time.sleep(1)
 
-soil_servo = Servo(25)
+
 # 土壌水分量測定モータで土壌水分センサを土に挿し込む
 print("土壌水分センサを土に挿し込む")
+soil_servo = Servo(25)
 try:
     soil_servo.rotate_cw_or_ccw(3.5)
     time.sleep(0.5)
@@ -50,6 +54,7 @@ time.sleep(1)
 
 # 土壌水分センサを機体に格納する
 print("土壌水分センサを機体に格納する")
+soil_servo = Servo(25)
 try:
     soil_servo.rotate_cw_or_ccw(11.5)
     time.sleep(0.5)
