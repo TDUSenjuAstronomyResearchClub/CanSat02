2320f9fe962a6d4fe5af5b986b8a870eaae3d9a3
Else156 <21ec109@ms.dendai.ac.jp>
2024-01-29 16:42:04 +0900

    i2cでもシリアル通信が切れるのか確認するためのコードを削除

--- running/main.py
+++ running/main.py
@@ -75,10 +75,6 @@ def detach_parachute():
     # time.sleep(10)
     # para_servo.rotate_stop()
 
-    while True:
-        acceleration_tmp = nineaxissensor.nine_axis_sensor.get_magnetic_heading()
-        print(acceleration_tmp)
-
     # 機体を前進させる
     dcmotor.Wheels.stop()
     print("機体を20秒前進させる")
